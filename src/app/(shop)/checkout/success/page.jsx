12833314ee92fff13f801767cbcf0be272c647b7
Ứng Hùng <111266351+unghung18@users.noreply.github.com>
2024-03-05 17:36:30 +0700

    update code

--- src/app/(shop)/checkout/success/page.jsx
+++ src/app/(shop)/checkout/success/page.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Image from 'next/image';
-import successIcon from '../../../../assets/success.png';
 
 const Page = () => {
     return (
         <div className='px-5 max-md:py-5 py-[120px] w-full text-center bg-slate-100'>
             <div className='flex flex-col items-center max-w-[500px] mx-auto bg-white p-5 rounded-md'>
-                <Image src={successIcon} width={150} alt='success icon image' />
+                <img src="/success.png" width={150} alt='success icon image' />
                 <h1 className='font-bold text-[22px] my-5'>Cảm ơn đã đặt hàng</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eius incidunt exercitationem, nobis doloremque quae nihil odio sit vero atque soluta similique voluptatibus labore earum iste. Perspiciatis fugiat animi iusto.</p>
                 <div className='flex justify-between items-center mt-10 space-x-5'>
